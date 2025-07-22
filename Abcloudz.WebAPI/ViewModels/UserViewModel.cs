e0a3861fc9a4522726eea6d57c22593d7f97ac59
Yurii Rudniev <yurii.rudniev@gmail.com>
2025-07-23 01:02:20 +0300

    Removed password field from response

--- Abcloudz.WebAPI/ViewModels/UserViewModel.cs
+++ Abcloudz.WebAPI/ViewModels/UserViewModel.cs
@@ -7,6 +7,5 @@ namespace Abcloudz.WebAPI.ViewModels
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
     }
 }
