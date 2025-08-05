5d5b43d66f5a24d193c07e61ba666c2979de4fb4
Maksym Leonenko <maksym.leonenko@diceus.com>
2025-08-05 19:35:19 +0300

    IPagedList + Filter class as Common infractructure has been created

---
+++ Abcloudz.WebAPI/Common/Filters/Filter.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Abcloudz.WebAPI.Common.Filters
+{
+	public class Filter<TFilter>
+	{
+		private int _pageSize = 10;
+		public int Page { get; set; } = 1;
+		public int PageSize
+		{
+			get => _pageSize;
+			set { _pageSize = value <= 0 ? 10 : _pageSize = value; }
+		}
+		public TFilter? Filters { get; set; }
+		public string OrderColumn { get; set; } = string.Empty;
+		public OrderSort Order { get; set; } = OrderSort.Desc;
+	}
+
+	[JsonConverter(typeof(JsonStringEnumConverter))]
+	public enum OrderSort
+	{
+		Asc,
+		Desc
+	}
+}
---
+++ Abcloudz.WebAPI/Common/IPagedList.cs
@@ -0,0 +1,13 @@
+﻿namespace Abcloudz.WebAPI.Common
+{
+	public interface IPagedList<T>
+	{
+		List<T> Items { get; }
+		int PageNumber { get; }
+		int PageSize { get; }
+		int TotalCount { get; }
+		int TotalPages { get; }
+		bool HasPreviousPage { get; }
+		bool HasNextPage { get; }
+	}
+}
